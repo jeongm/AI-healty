a39ee92e8e151cd9359aa067f0c262229bc7efe5
jeongm <jeongm0220@gmail.com>
2022-06-07 04:21:32 +0900

    flask test

---
+++ Flask_/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/main')
+def main():
+    # before login
+    return render_template('index.html')
+
+@app.route('/main/<user>')
+def main_user(user):
+    # after login
+    return render_template('index2.html',name=user)
+
+@app.route('/sign')
+def sign():
+    return render_template('sign.html')
+
+@app.route('/write')
+def write():
+    return render_template('write.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
